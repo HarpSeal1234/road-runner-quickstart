9fd099389cb0c153218a208c64253a514c72a13a
HarpSeal1234 <blueguh2010@gmail.com>
2025-12-07 18:50:05 -0500

    direction auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/posTurn.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;// RR-specific imports
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+@Autonomous(name = "positive turn 45", group = "Autonomous")
+
+public class posTurn extends LinearOpMode{
+    private static final boolean USE_WEBCAM = true;
+    public final static int FAR_OUTTAKE_VELOCITY = 1700;
+    public final static int CLOSE_OUTTAKE_VELOCITY = 1400;
+    public final static int MEDIUM_OUTTAKE_VELOCITY = 1200;
+
+    public void runOpMode() {
+        // instantiate your MecanumDrive at a particular pose.
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        AprilTagDetector aprilTagDetector = new AprilTagDetector(hardwareMap);
+        Intake intake1 = new Intake(hardwareMap);
+        Launcher outtake1 = new Launcher(hardwareMap);
+        Avocado blockerR = new Avocado(hardwareMap);
+        Avocado blockerL = new Avocado(hardwareMap);
+        Pivot pivot = new Pivot(hardwareMap);
+
+
+        int visionOutputPosition = 1;
+
+        TrajectoryActionBuilder path1 = drive.actionBuilder(initialPose)
+                .turn(Math.toRadians(45));
+        waitForStart();
+
+        if (isStopRequested()) return;
+        Action trajectoryActionChosen;
+        trajectoryActionChosen = path1.build();
+        Actions.runBlocking(new ParallelAction(trajectoryActionChosen)); // LEFT
+
+    }
+
+    }
+
+
+
+
