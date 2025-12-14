3ec9026a0300e618f6b6c27fe1281ddfffd4358b
HarpSeal1234 <blueguh2010@gmail.com>
2025-12-13 19:23:51 -0500

    spline tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/splineTangetn0.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+// RR-specific imports
+
+import static org.firstinspires.ftc.teamcode.OrcaRoboticsConstants.CLOSE_OUTTAKE_VELOCITY;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+@Autonomous(name = "spline 0", group = "Autonomous")
+
+public class splineTangetn0 extends LinearOpMode{
+    private static final boolean USE_WEBCAM = true;
+
+    public MecanumDrive drive ;
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(0));
+        drive = new MecanumDrive(hardwareMap, initialPose);
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        // TURN TO SHOOT
+        TrajectoryActionBuilder path2 = drive.actionBuilder(initialPose)
+                .fresh()
+                .splineToLinearHeading(new Pose2d(-24,24,Math.toRadians(5)),0);
+        Action trajectoryActionChosen2 = path2.build();
+
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionChosen2
+                )
+        );
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/splineTangetn45.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+// RR-specific imports
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+@Autonomous(name = "spline 45", group = "Autonomous")
+
+public class splineTangetn45 extends LinearOpMode{
+    private static final boolean USE_WEBCAM = true;
+
+    public MecanumDrive drive ;
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(0));
+        drive = new MecanumDrive(hardwareMap, initialPose);
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        // TURN TO SHOOT
+        TrajectoryActionBuilder path2 = drive.actionBuilder(initialPose)
+                .fresh()
+                .splineToLinearHeading(new Pose2d(-24,24,Math.toRadians(5)),Math.toRadians(45));
+        Action trajectoryActionChosen2 = path2.build();
+
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionChosen2
+                )
+        );
+    }
+}
