51918bb68657cecc565d6a402230ac63bd73babf
HarpSeal1234 <blueguh2010@gmail.com>
2026-01-10 18:44:46 -0500

    added auto locations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLocations.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+public class AutoLocations {
+    public static final double BLUE_CLOSE_SPIKE_ONE_FINAL_Y = -3;
+    public static final double BLUE_CLOSE_SPIKE_TWO_FINAL_Y = -5;
+    public static final double BLUE_CLOSE_SPIKE_ONE_MIN_VELOCITY = 16;
+    public static final double BLUE_CLOSE_SPIKE_TWO_MIN_VELOCITY = 15;
+    public static final double BLUE_CLOSE_FASTER_SPLINE_VELOCITY = 55;
+
+
+    public static final Vector2d BLUE_CLOSE_LEAVE = new Vector2d(-24,-10);
+    public static final Vector2d BLUE_CLOSE_SPIKE_ONE_START = new Vector2d(-29.0, -15);
+    public static final Vector2d BLUE_CLOSE_SPIKE_TWO_START = new Vector2d(-49, -24);
+
+    public static final Pose2d RED_BASKET_START = new Pose2d(-39, -63, Math.toRadians(-90));
+
+}
